0108a2b4702340a3ce989f56829cceec0822aa8b
Chris-Skipsey1 <112932687+Chris-Skipsey1@users.noreply.github.com>
2022-09-14 19:58:21 +0100

    Pages and layout (#1)
    * Pages
    
    * Added pages and layout components

---
+++ src/components/layouts/Header.js
@@ -0,0 +1,22 @@
+import './Header.css';
+
+function Header() {
+
+    return (
+        <header>
+            <a>
+                <img src="" alt="Cat" />
+            </a>
+            <a>
+            <h1>React First Build</h1>
+            </a>
+            <div className="login">
+            <p>Welcome Graeme!</p>
+            </div>
+        </header>
+    )
+
+
+}
+
+export default Header;

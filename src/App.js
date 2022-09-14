59b4fc39d101749cfc9565d916330f99d30dd618
Chris-Skipsey1 <K1915275@kingston.ac.uk>
2022-09-14 19:56:22 +0100

    Added pages and layout components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Layout from './components/layouts/Layout.js';
 import Home from './components/pages/Home.js';
+import SignIn from './components/pages/SignIn.js';
+import ContactUs from './components/pages/ContactUs.js';
+import PageNotFound from './components/pages/404.js';
+import './App.css';
 
 function App() {
   return (
+    <Layout>
     <Home />
-    
+    </Layout >
   );
 }
 
